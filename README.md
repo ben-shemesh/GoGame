33aef48c17c8db3e319542199723f69e91d3aa1a
avin brown <avinbrown@avins-Mac-mini.lan>
2022-10-21 09:19:47 -0400

    Added README.md.

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a repo that demonstrates the possibilities (not entirely ideal) of creating a very basic game in go. This is in no way comprehensive or display the most optimal way to create a game but
+displays the capabilities of using Go. Channels Go routines and pointers are displayed and to show what is possible.
+This is also a way for me to learn and track my progress in Go. 🐹
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ type Player struct {
     Health uint
     Name string
     AttackPower uint
+	Age uint
 }
 type Game struct {
 	isRunning bool
@@ -36,11 +37,21 @@ func newGame() *Game {
 		PlayerCh: make(chan *Player),
 	}
 }
+func fightPLayers(p1 *Player, p2 *Player){
+//	 health1 := p1.Health
+//	 health2 := p2.Health
+	if p1.Health * 2< p2.Health {
+		p2.Health = p2.Health/ 2
+		fmt.Printf("%s has lost %d health.\n" ,p2.Name, p2.Health)
+		if p2.Health == 0 {
+		fmt.Printf("%s has died", p2.Name)
+		}
+	}
+}
 func (g *Game) start() {
 	g.isRunning = true
 	g.gameLoop()
 }
-
 func (g *Game) gameLoop() {
     interval1 := 1 * time.Second
 	ticker := time.NewTicker(interval1)
@@ -56,39 +67,40 @@ func (g *Game) gameLoop() {
            g.isPause = true
         case <- ticker.C:
 		 	fmt.Println("Game is running")
-
         }
     }
 	fmt.Println("Bye Bye")
 }
-func  newPlayer(name string, hp uint, ap uint ) *Player {
+func  newPlayer(name string, hp uint, ap uint, ag uint ) *Player {
 	return &Player{
 		Health: hp,
 		Name: name,
 		AttackPower: ap,
+		Age: ag,
 		}
 }
 func (p *Player) dies()  {
 	p.Health = 0
 }
 
-func main() {
-	// move the start in a routine  / not correct usage but shows the point of Go routines
-	game := newGame()
-	playerA := newPlayer("Bob", 100,100)
-	playerB := newPlayer("Alice", 10001,101)
-	game.addPlayer(playerA)
-	game.addPlayer(playerB)
-	go addNewPlayer (game.PlayerCh)
-	game.start()
-
-}
 func stop(quitCh chan bool)  {
 	time.Sleep(time.Second * 5)
 	quitCh <- true
 }
 func  addNewPlayer (playersCh  chan *Player) {
 	time.Sleep(time.Second * 5)
-	player := newPlayer("JoeBuyDem", 50000, 1)
+	player := newPlayer("JoeBuyDem", 50000, 1, 29)
 	playersCh <- player
+}
+func main() {
+	// move the start in a routine  / not correct usage but shows the point of Go routines
+	game := newGame()
+	playerA := newPlayer("Bob", 100,100, 54)
+	playerB := newPlayer("Alice", 2000,101, 23)
+	game.addPlayer(playerA)
+	game.addPlayer(playerB)
+	go addNewPlayer (game.PlayerCh)
+	go fightPLayers(playerA, playerB)
+	game.start()
+
 }
