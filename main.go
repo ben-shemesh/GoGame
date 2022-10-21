539fa1bea2c9ca219f0f5764af333992f28e11ca
avin brown <avinbrown@avins-Mac-mini.lan>
2022-10-21 02:14:03 -0400

    need to add function to have players fight.

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
     "fmt"
     "time"
 )
-
 type Player struct {
 	// only positive health
     Health uint
@@ -13,35 +12,55 @@ type Player struct {
 }
 type Game struct {
 	isRunning bool
+	isPause bool
 	// the key is a string
 	// the value of a the map is a pointer to Player
 	players map[string]*Player
+	quitCh chan bool
+	pauseCh chan bool
+	PlayerCh chan *Player
 }
 func (g *Game) addPlayer(p *Player){
 	// links to the map
 	// stores Player by its name
 	g.players[p.Name] = p
-	fmt.Println("adding new players")
-
+	fmt.Printf("adding new players %s - %d\n", p.Name, p.AttackPower)
 }
 func newGame() *Game {
     return &Game{
 		isRunning: false,
+		isPause: false,
+		players: make(map[string]*Player),
+		quitCh:  make(chan bool),
+		pauseCh: make(chan bool),
+		PlayerCh: make(chan *Player),
 	}
 }
 func (g *Game) start() {
 	g.isRunning = true
 	g.gameLoop()
 }
+
 func (g *Game) gameLoop() {
     interval1 := 1 * time.Second
+	ticker := time.NewTicker(interval1)
+	fmt.Println("the game is running")
+	running:
 	for  {
-		fmt.Println("the game is running")
-		//
-		time.Sleep(interval1)
-    }
-}
+        select {
+        case player := <- g.PlayerCh:
+			g.addPlayer(player)
+        case <- g.quitCh:
+			break running
+        case <- g.pauseCh:
+           g.isPause = true
+        case <- ticker.C:
+		 	fmt.Println("Game is running")
 
+        }
+    }
+	fmt.Println("Bye Bye")
+}
 func  newPlayer(name string, hp uint, ap uint ) *Player {
 	return &Player{
 		Health: hp,
@@ -54,13 +73,22 @@ func (p *Player) dies()  {
 }
 
 func main() {
-	game := newGame()
 	// move the start in a routine  / not correct usage but shows the point of Go routines
-	go game.start()
+	game := newGame()
 	playerA := newPlayer("Bob", 100,100)
+	playerB := newPlayer("Alice", 10001,101)
 	game.addPlayer(playerA)
+	game.addPlayer(playerB)
+	go addNewPlayer (game.PlayerCh)
+	game.start()
 
-
-	fmt.Printf("%+v", game)
-
+}
+func stop(quitCh chan bool)  {
+	time.Sleep(time.Second * 5)
+	quitCh <- true
+}
+func  addNewPlayer (playersCh  chan *Player) {
+	time.Sleep(time.Second * 5)
+	player := newPlayer("JoeBuyDem", 50000, 1)
+	playersCh <- player
 }
