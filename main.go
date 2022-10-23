6d733468bed998a0ecf5e14c89370391263a8ce0
Avin Ben-Shemesh <ambrown111592@yahoo.com>
2022-10-23 02:11:01 -0400

    fixed the for loop.

--- main.go
+++ main.go
@@ -1,134 +1,72 @@
 package main
 
 import (
-	"fmt"
-	"time"
-	// "time"
+    "fmt"
+    "math/rand"
+    "time"
 )
+type Game struct {
+    isRunning bool
+}
+func (g *Game) start(){
+    g.gameLoop()
+    g.isRunning = true
+}
 
 type Player struct {
-	// only positive health
-    Health uint
     Name string
     AttackPower uint
-	Age uint
+    Health uint
 }
-type Game struct {
-	isRunning bool
-	isPause bool
-	// the key is a string
-	// the value of a the map is a pointer to Player
-	players map[string]*Player
-	quitCh chan bool
-	pauseCh chan bool
-	playerCh chan *Player
-	playerStatsCh chan *Player
+func addNewPLayer(pN string, pA uint , pH uint  ) *Player {
+    return &Player{
+        Name: pN,
+        AttackPower: pA,
+        Health: pH,
+    }
 }
-func (g *Game) addPlayer(p *Player){
-	// links to the map
-	// stores Player by p.Name
-	g.players[p.Name] = p
-	fmt.Printf("adding new players %s - %d\n", p.Name, p.AttackPower)
-}
-func newGame() *Game {
-    return &Game{
-		isRunning: false,
-		isPause: false,
-		players: make(map[string]*Player),
-		quitCh:  make(chan bool),
-		pauseCh: make(chan bool),
-		playerCh: make(chan *Player),
-	}
-}
-
-func fightPlayers(p1 *Player, p2 *Player) {
-	// var deadPlayer
-	fmt.Printf("%s's health is equal to %d\n",p1.Name, p1.Health)
-	fmt.Printf("%s's health is equal to %d\n",p2.Name, p2.Health)
-	if p1.Health * 2 < p2.Health {
-		p2.Health = p2.Health/ 2
-		fmt.Printf("%s has lost %d health.\n" ,p2.Name, p2.Health)
-		if p2.Health == 0 {
-			p2.dies()
-			fmt.Printf("%s has died\n", p2.Name)
-			// deadPlayer = p2
-		}
-	} else  {
-			fmt.Printf("%s has lost %d health.\n" ,p1.Name, p1.Health)
-			if p1.Health == 0 {
-				p1.dies()
-				fmt.Printf("%s has died\n", p1.Name)
-				// deadPlayer = p1 
-			}
-		}
-	// return deadPlayer
-}
-func (g *Game) start() {
-	g.isRunning = true
-	g.gameLoop()
-}
-func (g *Game) gameLoop() {
-    interval1 := 1 * time.Second
-	ticker := time.NewTimer(interval1)
-	fmt.Println("THE LOOP")
-
-	// causes the select to stop but the for loop contiunes to run
-	// named loop "running"
-		/// >>> starts "running loop"
-	running:
-	for {
-		select{
-		case <- g.quitCh:
-			// will make a break the select loop
-			break running ////// >>>> ends "running" loop
-		case <- g.pauseCh:
-			g.isPause = true
-		case <- ticker.C:
-			fmt.Println("the game is running")
-		}
-	}
-	fmt.Println("Bye Bye")
-}
-
-func newPlayer(name string, hp uint, ap uint, ag uint ) *Player {
-	return &Player{
-		Health: hp,
-		Name: name,
-		AttackPower: ap,
-		Age: ag,
-		}
-}
-func (p *Player) dies()  {
+func killPlayer(p *Player){
 	p.Health = 0
 }
-func stop(quitCh chan bool)  {
-	time.Sleep(time.Second * 5)
-	quitCh <- true
+
+func newGame() *Game {
+    return &Game{
+        isRunning: true,
+    }
 }
-func  addNewPlayer(playersCh  chan *Player) {
-	time.Sleep(time.Second * 5)
-	player := newPlayer("JoeBuyDem", 50000, 1, 29)
-	playersCh <- player
+func (g *Game) gameLoop(){
+    // is equal to 1 second occurance in program execuation
+    interval := time.Second * 1
+    timer := time.NewTimer(interval)
+    for {
+		fmt.Println("looping")
+		<- timer.C
+		timer.Reset(interval)
+	}
+	
 }
-func  displayThingsAfterFight(p *Player)  {
-	fmt.Printf("ALice's health is %d\n", p.Health)
+func (p *Player) powerDepleter() {
+    rand.Seed(time.Now().Unix())
+    rando := rand.Intn(5)
+    p.AttackPower = p.AttackPower / uint(rando)
 }
-func main() {
-	// move the start in a routine  / not correct usage but shows the point of Go routines
-	game := newGame()
-	playerA := newPlayer("Bob", 100,100, 54)
-	playerB := newPlayer("Alice", 2000,101, 23)
-	game.addPlayer(playerA)
-	game.addPlayer(playerB)
-	// go fightPlayers(playerA, playerB)
-	// go displayThingsAfterFight(playerB)
-	// go quite(game.quitCh)
-	 go addNewPlayer(game.playerCh)
-	game.start()
-	// test 
-}
-func quite (quitCh chan bool) {
-	time.Sleep(time.Second * 5)
-	quitCh <- true
+func (p *Player) powerBooster(){
+    rand.Seed(time.Now().Unix())
+    rando := rand.Intn(5)
+    p.Health = p.Health * uint(rando)
 }
 
+func main (){
+game := newGame()
+game.start()
+firstPLayer := addNewPLayer("Tony Balongi", 100, 100)
+// killPlayer(firstPLayer)
+// fmt.Println(firstPLayer)
+secondPLayer := addNewPLayer("Gloria Turning", 122, 8348)
+
+
+fmt.Printf("The players name is %s the players attack power is %d and the players health is %d\n",
+             firstPLayer.Name, firstPLayer.AttackPower,firstPLayer.Health )
+fmt.Printf("The players name is %s the players attack power is %d and the players health is %d\n",
+             secondPLayer.Name, secondPLayer.AttackPower,secondPLayer.Health )
+}
