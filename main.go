4f916e09f6b25c0d9d0bf1a90d05378783f418b8
Avin Ben-Shemesh <ambrown111592@yahoo.com>
2022-10-22 21:49:01 -0400

    moving on will understand better when i need to understand it better.

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-    "fmt"
-    "time"
+	"fmt"
+	"time"
+	// "time"
 )
+
 type Player struct {
 	// only positive health
     Health uint
@@ -19,11 +21,12 @@ type Game struct {
 	players map[string]*Player
 	quitCh chan bool
 	pauseCh chan bool
-	PlayerCh chan *Player
+	playerCh chan *Player
+	playerStatsCh chan *Player
 }
 func (g *Game) addPlayer(p *Player){
 	// links to the map
-	// stores Player by its name
+	// stores Player by p.Name
 	g.players[p.Name] = p
 	fmt.Printf("adding new players %s - %d\n", p.Name, p.AttackPower)
 }
@@ -34,30 +37,31 @@ func newGame() *Game {
 		players: make(map[string]*Player),
 		quitCh:  make(chan bool),
 		pauseCh: make(chan bool),
-		PlayerCh: make(chan *Player),
+		playerCh: make(chan *Player),
 	}
 }
 
-func fightPlayers(p1 *Player, p2 *Player)  uint {
-	var deadPlayer
-	fmt.Printf("Alice health is equal to %d\n", p2.Health)
+func fightPlayers(p1 *Player, p2 *Player) {
+	// var deadPlayer
+	fmt.Printf("%s's health is equal to %d\n",p1.Name, p1.Health)
+	fmt.Printf("%s's health is equal to %d\n",p2.Name, p2.Health)
 	if p1.Health * 2 < p2.Health {
 		p2.Health = p2.Health/ 2
 		fmt.Printf("%s has lost %d health.\n" ,p2.Name, p2.Health)
 		if p2.Health == 0 {
 			p2.dies()
 			fmt.Printf("%s has died\n", p2.Name)
-			deadPlayer = p2
+			// deadPlayer = p2
 		}
 	} else  {
 			fmt.Printf("%s has lost %d health.\n" ,p1.Name, p1.Health)
 			if p1.Health == 0 {
 				p1.dies()
 				fmt.Printf("%s has died\n", p1.Name)
-				deadPlayer = p1
+				// deadPlayer = p1 
 			}
 		}
-	return deadPlayer
+	// return deadPlayer
 }
 func (g *Game) start() {
 	g.isRunning = true
@@ -65,24 +69,28 @@ func (g *Game) start() {
 }
 func (g *Game) gameLoop() {
     interval1 := 1 * time.Second
-	ticker := time.NewTicker(interval1)
-	fmt.Println("the game is running")
+	ticker := time.NewTimer(interval1)
+	fmt.Println("THE LOOP")
+
+	// causes the select to stop but the for loop contiunes to run
+	// named loop "running"
+		/// >>> starts "running loop"
 	running:
-	for  {
-        select {
-        case player := <- g.PlayerCh:
-			g.addPlayer(player)
-        case <- g.quitCh:
-			break running
-        case <- g.pauseCh:
-           g.isPause = true
-        case <- ticker.C:
-		 	fmt.Println("Game is running")
-        }
-    }
+	for {
+		select{
+		case <- g.quitCh:
+			// will make a break the select loop
+			break running ////// >>>> ends "running" loop
+		case <- g.pauseCh:
+			g.isPause = true
+		case <- ticker.C:
+			fmt.Println("the game is running")
+		}
+	}
 	fmt.Println("Bye Bye")
 }
-func  newPlayer(name string, hp uint, ap uint, ag uint ) *Player {
+
+func newPlayer(name string, hp uint, ap uint, ag uint ) *Player {
 	return &Player{
 		Health: hp,
 		Name: name,
@@ -97,7 +105,7 @@ func stop(quitCh chan bool)  {
 	time.Sleep(time.Second * 5)
 	quitCh <- true
 }
-func  addNewPlayer (playersCh  chan *Player) {
+func  addNewPlayer(playersCh  chan *Player) {
 	time.Sleep(time.Second * 5)
 	player := newPlayer("JoeBuyDem", 50000, 1, 29)
 	playersCh <- player
@@ -112,9 +120,15 @@ func main() {
 	playerB := newPlayer("Alice", 2000,101, 23)
 	game.addPlayer(playerA)
 	game.addPlayer(playerB)
-	go fightPLayers(playerA, playerB)
-	go displayThingsAfterFight(playerB)
+	// go fightPlayers(playerA, playerB)
+	// go displayThingsAfterFight(playerB)
+	// go quite(game.quitCh)
+	 go addNewPlayer(game.playerCh)
 	game.start()
-	go addNewPlayer (game.PlayerCh)
 	// test 
 }
+func quite (quitCh chan bool) {
+	time.Sleep(time.Second * 5)
+	quitCh <- true
+}
+
